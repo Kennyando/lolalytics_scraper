55fb4af834bc62395646aca5bb00d64c8ab051d4
Kennyando <kennanneo@gmail.com>
2026-06-11 18:54:48 +0800

    lazy scroll method but need improve, about half champs inside

--- leaguescraper.py
+++ leaguescraper.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-
+import time
 #need html5,bs4,lxml,requests,pandas,numpy for webscraping,selenium,webdriver
 
 url_current = 'https://lolalytics.com/lol/tierlist/'
@@ -18,9 +18,25 @@ url_16_09 = 'https://lolalytics.com/lol/tierlist/?patch=16.9'
 driver = webdriver.Chrome()
 driver.get(url_current)
 #establish waiting strategy to make sure entire page is loaded
-WebDriverWait(driver, 15).until(
-    EC.presence_of_element_located((By.TAG_NAME, "body"))
-)
+#lazy scroll way(execute javascript to scroll)
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    #scroll to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(10)
+    for i in range (4):
+        driver.execute_script("window.scrollBy(0, -2000);")
+        time.sleep(5)
+        driver.execute_script("window.scrollBy(0,2000);")
+    #check if more stuff loaded within the 10secs
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
+
+
 #render html
 lolalytics_html = driver.page_source
 #exit selenium
@@ -29,7 +45,6 @@ driver.quit()
 soup = BeautifulSoup(lolalytics_html,"html.parser")
 individual_champs = soup.select("div[class *= 'flex h-[52px] justify-between text-[13px]']")
 
-print(individual_champs)
 # isn't sorted by table tag but rather a big div container
 # div class ="menu relative"
 # each champion is under a flex h-[52px] div as well
@@ -44,13 +59,9 @@ widths = {
     "PBI": "48px",
     "games": "72px"
 }
-#search via css selector
-champ_divs = soup.select("div[class *= 'menu:left-[30px]']")
 
-#key 2 to key 13, each key represents a set data,champ name,rank,wr,pick,ban rate
-    #for i in range(2,10):
-        #champ_stats = div.find("div", attrs={"q:key": f"{i}"})
-        #print(champ_stats.text)
-for div in individual_champs:
+#iterate through info to extract each data wanted
+for i,div in enumerate(individual_champs):
     #divs which have no repeating widths
     name = div.find("div", style=re.compile(widths["name"])).get_text(strip=True)
+    print(name,i)
