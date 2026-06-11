8605993866ca779827b718c3af103504671d3d0f
Kennyando <kennanneo@gmail.com>
2026-06-11 17:49:13 +0800

    selenium explicit wait time,not working .git/commit "selenium explicit wait time,not working"/

--- leaguescraper.py
+++ leaguescraper.py
@@ -1,19 +1,35 @@
 import numpy as np
 import pandas as pd
 import requests
-import lxml
 from bs4 import BeautifulSoup
 import re
-#need html5,bs4,lxml,requests,pandas,numpy for webscraping
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
-url_16_11 = 'https://lolalytics.com/lol/tierlist/'
+#need html5,bs4,lxml,requests,pandas,numpy for webscraping,selenium,webdriver
+
+url_current = 'https://lolalytics.com/lol/tierlist/'
 url_16_10 = 'https://lolalytics.com/lol/tierlist/?patch=16.10'
 url_16_09 = 'https://lolalytics.com/lol/tierlist/?patch=16.9'
 
-#webpage is loaded dynamically need a ghost loading like selenium to load the whole page before we parse
-webpage = requests.get(url_16_11)
-soup = BeautifulSoup(webpage.content,"html.parser")
+#loaded website using selenium
+driver = webdriver.Chrome()
+driver.get(url_current)
+#establish waiting strategy to make sure entire page is loaded
+WebDriverWait(driver, 15).until(
+    EC.presence_of_element_located((By.TAG_NAME, "body"))
+)
+#render html
+lolalytics_html = driver.page_source
+#exit selenium
+driver.quit()
 
+soup = BeautifulSoup(lolalytics_html,"html.parser")
+individual_champs = soup.select("div[class *= 'flex h-[52px] justify-between text-[13px]']")
+
+print(individual_champs)
 # isn't sorted by table tag but rather a big div container
 # div class ="menu relative"
 # each champion is under a flex h-[52px] div as well
@@ -30,8 +46,7 @@ widths = {
 }
 #search via css selector
 champ_divs = soup.select("div[class *= 'menu:left-[30px]']")
-individual_champs = soup.select("div[class *= 'flex h-[52px] justify-between text-[13px]']")
-print(individual_champs)
+
 #key 2 to key 13, each key represents a set data,champ name,rank,wr,pick,ban rate
     #for i in range(2,10):
         #champ_stats = div.find("div", attrs={"q:key": f"{i}"})
